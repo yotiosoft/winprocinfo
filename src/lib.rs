6dada7e512728fb3d29179066b313dfc6b61f18b
yotiosoft <yotio@yotiosoft.com>
2025-02-27 02:25:51 +0900

    impl safe buffer

--- src/lib.rs
+++ src/lib.rs
@@ -46,12 +46,53 @@ struct BufferStruct {
     base_address: *mut c_void,
     alloc_size: usize,
 }
+impl Drop for BufferStruct {
+    fn drop(&mut self) {
+        vfree(self.base_address, self.alloc_size);
+    }
+}
 
 pub fn get() -> Result<WinProcList, WinProcListError> {
     let buffer = get_system_processes_info()?;
     let list_vec = get_proc_list(buffer.base_address);
-    vfree(buffer.base_address, buffer.alloc_size);
     Ok(WinProcList { proc_list: list_vec })
+}
+
+pub fn get_proc_info_by_pid(pid: u32) -> Result<Option<ProcInfo>, WinProcListError> {
+    let buffer = get_system_processes_info()?;
+    let mut system_process_information = read_proc_info(buffer.base_address as isize);
+    let mut next_address = buffer.base_address as isize;
+
+    loop {
+        next_address += system_process_information.NextEntryOffset as isize;
+        system_process_information = read_proc_info(next_address);
+
+        if system_process_information.UniqueProcessId as u32 != pid {
+            if system_process_information.NextEntryOffset == 0 {
+                break;
+            }
+            continue;
+        }
+
+        let proc_info: ProcInfo = ProcInfo {
+            image_name: get_str_from_mem(system_process_information.ImageName.Buffer as *mut c_void, 0, system_process_information.ImageName.Length as usize),
+            unique_process_id: system_process_information.UniqueProcessId as u32,
+            handle_count: system_process_information.HandleCount,
+            session_id: system_process_information.SessionId,
+            peak_virtual_size: system_process_information.PeakVirtualSize,
+            virtual_size: system_process_information.VirtualSize,
+            peak_working_set_size: system_process_information.PeakWorkingSetSize,
+            quota_paged_pool_usage: system_process_information.QuotaPagedPoolUsage,
+            quota_non_paged_pool_usage: system_process_information.QuotaNonPagedPoolUsage,
+            pagefile_usage: system_process_information.PagefileUsage,
+            peak_pagefile_usage: system_process_information.PeakPagefileUsage,
+            private_page_count: system_process_information.PrivatePageCount,
+        };
+
+        return Ok(Some(proc_info));
+    }
+
+    Ok(None)
 }
 
 // 現在動作中のすべてのプロセス情報を取得
@@ -72,10 +113,7 @@ fn get_system_processes_info() -> Result<BufferStruct, WinProcListError> {
             status = NtQuerySystemInformation(SystemProcessInformation, base_address, buffer_size as u32, &mut buffer_size as *mut u32);
         }
         if NT_ERROR(status) {
-            if status == STATUS_INFO_LENGTH_MISMATCH || status == STATUS_BUFFER_TOO_SMALL {
-                vfree(base_address, buffer_size as usize);
-            }
-            else {
+            if status != STATUS_INFO_LENGTH_MISMATCH && status != STATUS_BUFFER_TOO_SMALL {
                 return Err(WinProcListError::CouldNotGetProcInfo(status));
             }
         }
@@ -128,26 +166,15 @@ fn get_proc_list(base_address: *mut c_void) -> Vec<ProcInfo> {
 fn read_proc_info(next_address: isize) -> SYSTEM_PROCESS_INFORMATION {
     unsafe {
         let mut system_process_info: SYSTEM_PROCESS_INFORMATION = std::mem::zeroed();
-
         // base_address の該当オフセット値から SYSTEM_PROCESS_INFORMATION 構造体の情報をプロセス1つ分取得
-        ReadProcessMemory(
-            GetCurrentProcess(), next_address as *const c_void, &mut system_process_info as *mut _ as *mut c_void, 
-            std::mem::size_of::<SYSTEM_PROCESS_INFORMATION>() as usize, std::ptr::null_mut()
-        );
-
+        read_process_memory(next_address as *mut c_void, &mut system_process_info as *mut _ as *mut c_void, std::mem::size_of::<SYSTEM_PROCESS_INFORMATION>());
         system_process_info
     }
 }
 
 fn get_str_from_mem(base_address: *mut c_void, offset: usize, size: usize) -> String {
     let mut vec: Vec<u16> = vec![0; size];
-    unsafe {
-        ReadProcessMemory(
-            GetCurrentProcess(), (base_address as usize + offset) as *const c_void, vec.as_mut_ptr() as *mut c_void, 
-            size, std::ptr::null_mut()
-        );
-    }
-
+    read_process_memory((base_address as usize + offset) as *mut c_void, vec.as_mut_ptr() as *mut c_void, size);
     String::from_utf16_lossy(&vec).trim_matches(char::from(0)).to_string()
 }
 
@@ -165,41 +192,10 @@ fn vfree(addr: *mut c_void, buffer_size: usize) {
     }
 }
 
-pub fn get_proc_info_by_pid(pid: u32) -> Result<Option<ProcInfo>, WinProcListError> {
-    let buffer = get_system_processes_info()?;
-    let mut system_process_information = read_proc_info(buffer.base_address as isize);
-    let mut next_address = buffer.base_address as isize;
-
-    loop {
-        next_address += system_process_information.NextEntryOffset as isize;
-        system_process_information = read_proc_info(next_address);
-
-        if system_process_information.UniqueProcessId as u32 != pid {
-            if system_process_information.NextEntryOffset == 0 {
-                break;
-            }
-            continue;
-        }
-
-        let proc_info: ProcInfo = ProcInfo {
-            image_name: get_str_from_mem(system_process_information.ImageName.Buffer as *mut c_void, 0, system_process_information.ImageName.Length as usize),
-            unique_process_id: system_process_information.UniqueProcessId as u32,
-            handle_count: system_process_information.HandleCount,
-            session_id: system_process_information.SessionId,
-            peak_virtual_size: system_process_information.PeakVirtualSize,
-            virtual_size: system_process_information.VirtualSize,
-            peak_working_set_size: system_process_information.PeakWorkingSetSize,
-            quota_paged_pool_usage: system_process_information.QuotaPagedPoolUsage,
-            quota_non_paged_pool_usage: system_process_information.QuotaNonPagedPoolUsage,
-            pagefile_usage: system_process_information.PagefileUsage,
-            peak_pagefile_usage: system_process_information.PeakPagefileUsage,
-            private_page_count: system_process_information.PrivatePageCount,
-        };
-
-        vfree(buffer.base_address, buffer.alloc_size);
-        return Ok(Some(proc_info));
+fn read_process_memory(base_address: *mut c_void, buffer: *mut c_void, buffer_size: usize) {
+    unsafe {
+        ReadProcessMemory(
+            GetCurrentProcess(), base_address, buffer, buffer_size, std::ptr::null_mut()
+        );
     }
-
-    vfree(buffer.base_address, buffer.alloc_size);
-    Ok(None)
 }
